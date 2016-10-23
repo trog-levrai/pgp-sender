3e7ba35719f750ba2e43d76fd35626229bd5401a
Ilan Dubois <idubois@advanced-schema.com>
2016-10-23 15:35:45 +0200

    Moves script.py to src directory and adds shabang

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # pgp-sender
 PGPSender sends PGP encryptes emails to a csv list of keyID/adress
 Please find the required packages to run the script in requirements.txt
+The main script is in the src directory: script.py
+For a complete list of options:
+'''shell
+./script.py --help
+'''
--- src/script.py
+++ src/script.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python3
+
 from optparse import OptionParser
-import src.mail
-import src.encrypt
+import mail
+import encrypt
 import csv
 
 if __name__ == "__main__":
     parser = OptionParser(description="This script is used to send GPG encrypted emails to a list of recipients.")
     parser.add_option("-f", "--file", dest="file", help="CSV file containing the list. The first column should have Key IDs and the second one the adress to send to", metavar="FILE", default="")
     parser.add_option("-s", "--subject", dest="subject", help="sets the email subject", metavar="STR", default="")
+    parser.add_option("-a", "--address", dest="address", help="sets the email sender", metavar="STR", default="foo@bar.baz")
     args, trash = parser.parse_args()
+
     with open(args.file, 'r') as csvFile:
-        gpgEngine = src.encrypt.GPGEngine()
+        gpgEngine = encrypt.GPGEngine()
         csvData = csv.reader(csvFile, delimiter=',')
         for i in csvData:
             message = str(gpgEngine.encryptMail("Mail sent using GPG", i[0]))
-            src.mail.sendMail("foo@test.com", i[1], args.subject, message)
+            mail.sendMail(args.address, i[1], args.subject, message)
